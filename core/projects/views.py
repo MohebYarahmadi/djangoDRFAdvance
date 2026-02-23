d0e3415bbd4330e3c0a4934135bc4a71434fe9fe
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-02-23 23:06:49 +0330

    Rendering data to templates done

--- core/projects/views.py
+++ core/projects/views.py
@@ -5,9 +5,18 @@ from django.http import HttpResponse
 # Create your views here.
 def projects(request):
     template_name = 'projects/projects.html'
-    return render(request, template_name)
+    context = {
+        'first_name': 'John',
+        'last_name': 'Doe',
+    }
+    return render(request, template_name, context)
 
 
-def project(request):
+def project(request, pk):
     template_name = 'projects/single-project.html'
-    return render(request, template_name)
+    context = {
+        'pk': pk,
+        'title': 'My Game',
+        'budget': '5M',
+    }
+    return render(request, template_name, context)
