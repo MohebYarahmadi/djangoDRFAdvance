ed5c83c5b5a58692d40dea494b5082a76004e009
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-02-25 22:04:31 +0330

    Database Queries Done

--- core/projects/views.py
+++ core/projects/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from . import models
 
 # Create your views here.
 def projects(request):
     template_name = 'projects/projects.html'
-    context = {
-        'first_name': 'John',
-        'last_name': 'Doe',
-    }
+    all_projects = models.Project.objects.all()
+    context = { 'projecs': all_projects }
     return render(request, template_name, context)
 
 
 def project(request, pk):
     template_name = 'projects/single-project.html'
+    single_project = models.Project.objects.get(id=pk)
+    tags = single_project.tags.all()
     context = {
         'pk': pk,
-        'title': 'My Game',
-        'budget': '5M',
+        'project': single_project,
+        'tags': tags,
     }
     return render(request, template_name, context)
